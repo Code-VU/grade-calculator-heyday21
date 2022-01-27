64dca019e2b32fedae557cb68214f2c7c2e45ea3
Dana Schmidt <Dana.Schmidt@vu.com>
2022-01-27 15:34:44 -0600

    messed up everything by attempting a try/except

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,30 +2,32 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     # This first line is provided for you
     print("Calculating Grade")
-    score =(input("Enter Score: "))
+    grade =input("Enter Score: ")
     try:
-        grade = float(score)
+        float(grade)
+        try:
+            if grade >.9999:
+                print ("Bad score")
+        elif (1.0> grade >=.9):
+            print ("A")
+        elif (.9> grade >=.8):
+            print ("B")
+        elif (.8> grade >=.7):
+            print ("C")
+        elif (.7 >grade >=.6):
+            print ("D")
+        elif (0 <=grade <.6):
+            print ("F")
+        elif grade <0:
+            print ("Bad score")
+        except:
+            print("Bad score")
     except:
-        print("Bad grade")
-    if grade >.9999:
-        print ("Bad Grade")
-    elif (1.0> grade >=.9):
-        print ("A")
-    elif (.9> grade >=.8):
-        print ("B")
-    elif (.8> grade >=.7):
-        print ("C")
-    elif (.7 >grade >=.6):
-        print ("D")
-    elif (0 <=grade <.6):
-        print ("F")
-    elif grade <0:
-        print ("Bad Grade")
-    
+        print ("bad score")
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateGrade() and run > python calculateGrade.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-#calculateGrade()
+calculateGrade()
