4fd55f2c6bdf1679b0982f9cd8481a58725dbe93
Dana Schmidt <Dana.Schmidt@vu.com>
2022-02-03 13:18:50 -0600

    fixed order of if elif statements

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,28 +2,23 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     # This first line is provided for you
     print("Calculating Grade")
-    grade =input("Enter Score: ")
-    try:
-        float(grade)
-        try:
-            if grade >.9999:
-                print ("Bad score")
-        elif (1.0> grade >=.9):
-            print ("A")
-        elif (.9> grade >=.8):
-            print ("B")
-        elif (.8> grade >=.7):
-            print ("C")
-        elif (.7 >grade >=.6):
-            print ("D")
-        elif (0 <=grade <.6):
-            print ("F")
-        elif grade <0:
-            print ("Bad score")
-        except:
-            print("Bad score")
-    except:
-        print ("bad score")
+    grade = float(input("Enter Score: "))
+   
+    if grade <0:
+        print ("Bad score")
+    elif (grade <.6):
+        print ("F")
+    elif (grade <.7):
+        print ("D")
+    elif (grade <.8):
+        print ("C")
+    elif (grade <.9):
+        print ("B")
+    elif (.9 <=grade <=1.0):
+        print ("A")
+    else:
+        print ("Bad score")
+        
     # end assignment
 
 ## if you want to test locally before you try to sync
